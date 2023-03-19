c349cabd8c84d54dff9de02299d7982c0fc88eb8
lokyaloky <y.alok51053@gmail.com>
2023-03-20 00:38:40 +0530

    assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Card, CardContent, CardMedia, Grid, Typography } from "@mui/material";
+import styled from "styled-components";
+
+const StyledCard = styled(Card)`
+  width: 100%;
+  height: 100%;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.5);
+  transition: 0.6s;
+  border-radius: 10px;
+  
+  &:hover {
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0,0.9);
+  }
+`;
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/posts')
+      .then(response => response.json())
+      .then(data => {
+        if (Array.isArray(data.posts)) {
+          setPosts(data.posts);
+        } else {
+          console.error('API response is not an array:', data);
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching posts:', error);
+      });
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Posts</h1>
+      <Grid container spacing={2}>
+        {posts.map((post) => (
+          <Grid item xs={12} sm={6} md={4} key={post.id}>
+            <StyledCard>
+              <CardMedia
+                component="img"
+                height="200"
+                image={`https://picsum.photos/id/${post.id}/200/200`}
+                alt="placeholder"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {post.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {post.body}
+                </Typography>
+              </CardContent>
+            </StyledCard>
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 }
